b6474390e03b83fae665dde22ef75d16cbe78823
kev127 <kelvinkeya125@gmail.com>
2020-10-17 11:20:51 +0300

    add parameters for news objects

--- app/models/news.py
+++ app/models/news.py
@@ -11,3 +11,14 @@ class News:
         self.category = category
         self.country = country
         self.language = language
+        
+class Articles:
+    '''
+    Articles class to define Articles object 
+    '''
+    def __init__(self, title,descripion, url, urlToImage, publishedAt):
+        self.title = title 
+        self.descripion = descripion
+        self.url =url 
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
