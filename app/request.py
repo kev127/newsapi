e5f34e7b1f252c7a95f419ae08c4defaff837993
kev127 <kelvinkeya125@gmail.com>
2020-10-20 21:58:14 +0300

    update code

--- app/request.py
+++ app/request.py
@@ -1,13 +1,15 @@
 import urllib.request,json
-from .models import Sources
+from .models import Sources,Articles
 
 # Getting api key
 api_key = None
-# Getting the movie base url
+# Getting the news base url
 base_url = None
+# Getting the articles base url
+articles_base_url = None
 
 def configure_request(app):
-    global api_key,base_url
+    global api_key,base_url,articles_base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     articles_base_url = app.config['ARTICLES_BASE_URL']
