73d12777269227798c4e11218342924195d14454
kev127 <kelvinkeya125@gmail.com>
2020-10-20 13:25:34 +0300

    update config.py file

--- app/config.py
+++ app/config.py
@@ -4,23 +4,16 @@ class Config:
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
